fec483080c05ca9c3ee008cbc10a64e94daba2a0
Jeff Brunson <jeff.a.brunson@gmail.com>
2015-10-31 13:06:32 -0600

    navbar

---
+++ BookList.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+  StyleSheet,
+  View,
+  Text,
+  Component
+} = React;
+
+var styles = StyleSheet.create({
+
+});
+
+class BookList extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
+module.exports = BookList;
